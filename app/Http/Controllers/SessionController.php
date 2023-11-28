09eea16efe470a158c71735bf0b2610969f6883a
Miguel Herrera <miguelsot959@hotmail.com>
2023-11-28 05:08:24 -0400

    Update start session

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SessionController extends Controller
 {
@@ -15,14 +16,17 @@ class SessionController extends Controller
 	public function login(Request $request)
 	{
 		// Consultamos la información del usuario.
-		$user	= Session::where('usuario', $request->username)->first();
+		$user	= DB::table('tb_usuarios')
+			->join('tb_personal', 'tb_usuarios.cedula', '=', 'tb_personal.cedula')
+			->select('tb_personal.*', 'tb_usuarios.usuario', 'tb_usuarios.id_usuario', 'tb_usuarios.estatus', 'tb_usuarios.contrasena')
+			->where('usuario', $request->username)->first();
 
 		// Validamos si encontró el usuario solicitado.
 		if (!$user) return redirect('iniciar-sesion')->with('error', 'El usuario no se encuentra registrado');
 
 		// Comprobamos que la contraseña sea correcta.
 		if (!password_verify($request->password, $user->contrasena)) return redirect('iniciar-sesion')->with('error', 'La contraseña ingresada es incorrecta');
-		
+
 		// Guardamos la sesión y redireccionamos al panel principal.
 		session(['user' => $user]);
 		return redirect('/');
